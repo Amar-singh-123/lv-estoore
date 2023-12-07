745720b1a1f88388f9de34b09f69c5f666102029
Amar Singh <amrnth12123@gmail.com>
2023-12-07 18:10:12 +0530

    added a get api

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+   public function index(){
+    $data = [
+        [
+            "id" => "01",
+            "name" => "amar singh",
+            "age" => "18",
+        ],
+        [
+            "id" => "02",
+            "name" => "Ram singh",
+            "age" => "18",
+        ],
+        [
+            "id" => "03",
+            "name" => "Raushan singh",
+            "age" => "18",
+        ],
+        [
+            "id" => "04",
+            "name" => "Vijay singh",
+            "age" => "18",
+        ],
+    ];
+
+    return $data;
+  }
+}
